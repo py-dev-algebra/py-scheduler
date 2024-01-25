dda3411f2fd9daf741c64fa7e41fd85e18ca9ec0
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-01-25 19:20:48 +0100

    Summary

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 from models.customers import Customer
 from models.appointments import Appointment
 
 
 
+
+appointment = Appointment('Demonstracija novog proizvoda',
+                          )
--- models/appointments.py
+++ models/appointments.py
@@ -1,5 +1,41 @@
+from datetime import datetime
+from datetime import timedelta
+from models.customers import Customer
+
+'''
+- vrijeme
+- mjesto
+- osobe
+- datum
+- naslov / tema / topic
+- ponavljanja
+'''
 
 
 
 class Appointment:
-    pass
+    def __init__(self,
+                 title: str,
+                 start: datetime,
+                 duration: timedelta,
+                 end: datetime = datetime(1900, 1, 1),
+                 attendees: list[Customer] = [],
+                 location: str = '',
+                 recurrent: int = 0) -> None:
+        self.start = start
+        self.end = end
+        self.duration = duration
+        self.title = title
+        self.attendees = attendees
+        self.location = location
+        self.recurrent = recurrent
+
+        self.calculate_end()
+
+
+    def calculate_end(self):
+        self.end = self.start + self.duration
+
+
+    # def __str__(self):
+    #     pass
